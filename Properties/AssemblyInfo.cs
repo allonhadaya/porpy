bfa951b93a9d887f699e5682ff16b33488a5eaf9
Allon Hadaya <allonhadaya@hotmail.com>
2012-10-23 19:12:26 -0400

    Initial commit. Empty class library. Ignore file.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClientFramework")]
+[assembly: AssemblyDescription("A framework for building client libraries to web services.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClientFramework")]
+[assembly: AssemblyCopyright("Copyright © Allon Hadaya 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1f7f3d30-e452-47d4-a4e3-4293d93af857")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
